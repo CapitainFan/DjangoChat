1fe5f51bd66018b000541d1de1e034aa69f804fe
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2024-02-02 23:40:53 +0300

    need to fix some bugs with web-sokets

--- djangochat/djangochat/wsgi.py
+++ djangochat/djangochat/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for djangochat project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import room.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 
-application = get_wsgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            room.routing.websocket_urlpatterns
+        )
+    )
+})
